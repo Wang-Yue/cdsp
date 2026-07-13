ada7992430a51a89e76614386cf9c6feee3d3dac
Wang-Yue <yuleopen@gmail.com>
2026-07-13 09:40:00 +0900

    refactor: reorder cdsp_clock_gettime_nsec_np function in clock_mock.c

--- Tests/CLibTests/clock_mock.c
+++ Tests/CLibTests/clock_mock.c
@@ -18,22 +18,6 @@ static void init_clock_mock(void) {
   s_mock_start_time_ns = s_real_start_time_ns;
 }
 
-uint64_t cdsp_clock_gettime_nsec_np(clockid_t clock_id) {
-#ifdef __APPLE__
-  if (s_real_start_time_ns == 0) {
-    init_clock_mock();
-  }
-  uint64_t real_now = clock_gettime_nsec_np(clock_id);
-  uint64_t elapsed = real_now - s_real_start_time_ns;
-  return s_mock_start_time_ns + elapsed * 15;
-#else
-  (void)clock_id;
-  struct timespec ts;
-  cdsp_clock_gettime(CLOCK_MONOTONIC, &ts);
-  return (uint64_t)ts.tv_sec * 1000000000ULL + ts.tv_nsec;
-#endif
-}
-
 int cdsp_clock_gettime(clockid_t clock_id, struct timespec* tp) {
   if (s_real_start_time_ns == 0) {
     init_clock_mock();
@@ -51,6 +35,22 @@ int cdsp_clock_gettime(clockid_t clock_id, struct timespec* tp) {
   tp->tv_sec = (time_t)(fake_now / 1000000000ULL);
   tp->tv_nsec = (long)(fake_now % 1000000000ULL);
   return 0;
+}
+
+uint64_t cdsp_clock_gettime_nsec_np(clockid_t clock_id) {
+#ifdef __APPLE__
+  if (s_real_start_time_ns == 0) {
+    init_clock_mock();
+  }
+  uint64_t real_now = clock_gettime_nsec_np(clock_id);
+  uint64_t elapsed = real_now - s_real_start_time_ns;
+  return s_mock_start_time_ns + elapsed * 15;
+#else
+  (void)clock_id;
+  struct timespec ts;
+  cdsp_clock_gettime(CLOCK_MONOTONIC, &ts);
+  return (uint64_t)ts.tv_sec * 1000000000ULL + ts.tv_nsec;
+#endif
 }
 
 int cdsp_nanosleep(const struct timespec* req, struct timespec* rem) {
