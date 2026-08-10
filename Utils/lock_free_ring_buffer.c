08833bef35eadc5d19a1e912928c1df661635704
Wang-Yue <yuleopen@gmail.com>
2026-08-10 18:46:16 +0900

    fix: update memory order for atomic load in spsc_audio_ring_buffer_read_latest_at function

--- Utils/lock_free_ring_buffer.c
+++ Utils/lock_free_ring_buffer.c
@@ -404,9 +404,8 @@ bool spsc_audio_ring_buffer_read_latest_at(const spsc_audio_ring_buffer_t* ring,
              (count - first_chunk) * sizeof(float));
     }
 
-    atomic_thread_fence(memory_order_acquire);
     uint64_t seq_after =
-        atomic_load_explicit(&ring->write_seq, memory_order_relaxed);
+        atomic_load_explicit(&ring->write_seq, memory_order_acquire);
     if (seq_after == seq_before) {
       return true;
     }
