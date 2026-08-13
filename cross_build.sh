dd5459fe777ed8c0df559500d70e3624230b6040
Wang-Yue <yuleopen@gmail.com>
2026-08-14 03:37:32 +0900

    feat: enable USE_LIBDISPATCH in cross-compilation script for enhanced functionality

--- cross_build.sh
+++ cross_build.sh
@@ -14,9 +14,10 @@ make CC="$CC" \
      ENABLE_ALSA=1 \
      ENABLE_PIPEWIRE=1 \
      ENABLE_FFTW=1 \
+     USE_LIBDISPATCH=1 \
      IS_DARWIN=0 \
-     CFLAGS="--sysroot=$SYSROOT -isystem $SYSROOT/usr/include/aarch64-linux-gnu -isystem $SYSROOT/usr/include/dbus-1.0 -isystem $SYSROOT/usr/lib/aarch64-linux-gnu/dbus-1.0/include -isystem $SYSROOT/usr/include/pipewire-0.3 -isystem $SYSROOT/usr/include/spa-0.2 -D_REENTRANT -O3 -std=c11 -DCDSP_BUILD_SHARED -Wall -Wextra -DENABLE_ALSA -DENABLE_PIPEWIRE -DENABLE_FFTW -D_GNU_SOURCE -I. -I./Filters -I./Audio -I./Config -I./FFT -I./Mixer -I./Resampler -I./Processors -I./DoP -I./Pipeline -I./Engine -I./Server -I./Backend -I./Logging -I./Utils" \
-     LDFLAGS="--sysroot=$SYSROOT -B$SYSROOT/usr/lib/aarch64-linux-gnu -L$SYSROOT/usr/lib/aarch64-linux-gnu -L$SYSROOT/lib/aarch64-linux-gnu -Wl,-rpath-link,$SYSROOT/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,$SYSROOT/lib/aarch64-linux-gnu -lm -lpthread -ldbus-1 -lasound -lpipewire-0.3 -lfftw3 -lfftw3f -lrt" \
+     CFLAGS="--sysroot=$SYSROOT -isystem $SYSROOT/usr/include/aarch64-linux-gnu -isystem $SYSROOT/usr/include/dbus-1.0 -isystem $SYSROOT/usr/lib/aarch64-linux-gnu/dbus-1.0/include -isystem $SYSROOT/usr/include/pipewire-0.3 -isystem $SYSROOT/usr/include/spa-0.2 -isystem $SYSROOT/usr/libexec/swift/lib/swift -D_REENTRANT -O3 -std=c11 -DCDSP_BUILD_SHARED -Wall -Wextra -DENABLE_ALSA -DENABLE_PIPEWIRE -DENABLE_FFTW -DUSE_LIBDISPATCH -D_GNU_SOURCE -I. -I./Filters -I./Audio -I./Config -I./FFT -I./Mixer -I./Resampler -I./Processors -I./DoP -I./Pipeline -I./Engine -I./Server -I./Backend -I./Logging -I./Utils" \
+     LDFLAGS="--sysroot=$SYSROOT -B$SYSROOT/usr/lib/aarch64-linux-gnu -L$SYSROOT/usr/lib/aarch64-linux-gnu -L$SYSROOT/lib/aarch64-linux-gnu -L$SYSROOT/usr/libexec/swift/lib/swift/linux -Wl,-rpath-link,$SYSROOT/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,$SYSROOT/lib/aarch64-linux-gnu -Wl,-rpath-link,$SYSROOT/usr/libexec/swift/lib/swift/linux -Wl,-rpath,/usr/libexec/swift/lib/swift/linux -lm -lpthread -ldbus-1 -lasound -lpipewire-0.3 -lfftw3 -lfftw3f -ldispatch -lBlocksRuntime -lrt" \
      -j8 "$@"
 
 echo "✅ Cross-compilation complete: bin/dsp-cli"
