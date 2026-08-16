3b955b435de47e1e54c51fea5314f3c38795c886
Wang-Yue <yuleopen@gmail.com>
2026-08-16 07:20:16 +0000

    feat: enhance ALSA capabilities with support for channel and sample rate queries

--- Backend/alsa_capabilities.c
+++ Backend/alsa_capabilities.c
@@ -18,8 +18,7 @@
 static const unsigned int STANDARD_RATES[] = {
     5512,  8000,  11025,  16000,  22050,  32000,  44100,  48000, 64000,
     88200, 96000, 176400, 192000, 352800, 384000, 705600, 768000};
-static const size_t STANDARD_RATES_COUNT =
-    sizeof(STANDARD_RATES) / sizeof(STANDARD_RATES[0]);
+#define STANDARD_RATES_COUNT (sizeof(STANDARD_RATES) / sizeof(STANDARD_RATES[0]))
 
 static const unsigned int CAPABILITY_PROBE_CHANNEL_LIMIT = 128;
 
@@ -175,6 +174,110 @@ static void alsa_sanitize_device_name(const char* in_name, char* out_name,
   }
 }
 
+// Supported channel values matching supported_channel_values in upstream
+// (src/alsa_backend/utils.rs:342-359)
+static int supported_channel_values(snd_pcm_t* pcm, snd_pcm_hw_params_t* hwp,
+                                    unsigned int limit,
+                                    unsigned int* out_channels,
+                                    size_t max_out) {
+  unsigned int min_channels = 0, max_channels = 0;
+  if (snd_pcm_hw_params_get_channels_min(hwp, &min_channels) < 0 ||
+      snd_pcm_hw_params_get_channels_max(hwp, &max_channels) < 0) {
+    return -1;
+  }
+  if (min_channels == max_channels) {
+    if (max_out > 0) {
+      out_channels[0] = min_channels;
+      return 1;
+    }
+    return 0;
+  }
+
+  unsigned int check_max = max_channels < limit ? max_channels : limit;
+  size_t count = 0;
+  for (unsigned int chan = min_channels; chan <= check_max && count < max_out;
+       chan++) {
+    if (snd_pcm_hw_params_test_channels(pcm, hwp, chan) == 0) {
+      out_channels[count++] = chan;
+    }
+  }
+  return (int)count;
+}
+
+// Supported rate values matching SupportedValues & list_samplerates in
+// upstream (src/alsa_backend/utils.rs:41-44 & 312-331)
+typedef enum {
+  SUPPORTED_RATES_RANGE,
+  SUPPORTED_RATES_DISCRETE
+} supported_rates_type_t;
+
+typedef struct {
+  supported_rates_type_t type;
+  unsigned int min;
+  unsigned int max;
+  unsigned int discrete[STANDARD_RATES_COUNT];
+  size_t discrete_count;
+} supported_rates_t;
+
+static bool list_samplerates(snd_pcm_t* pcm, snd_pcm_hw_params_t* hwp,
+                             supported_rates_t* out_rates) {
+  unsigned int min_rate = 0, max_rate = 0;
+  if (snd_pcm_hw_params_get_rate_min(hwp, &min_rate, NULL) < 0 ||
+      snd_pcm_hw_params_get_rate_max(hwp, &max_rate, NULL) < 0) {
+    return false;
+  }
+  if (min_rate == max_rate) {
+    out_rates->type = SUPPORTED_RATES_DISCRETE;
+    out_rates->discrete[0] = min_rate;
+    out_rates->discrete_count = 1;
+    return true;
+  } else if (snd_pcm_hw_params_test_rate(pcm, hwp, min_rate + 1, 0) == 0) {
+    out_rates->type = SUPPORTED_RATES_RANGE;
+    out_rates->min = min_rate;
+    out_rates->max = max_rate;
+    return true;
+  }
+  out_rates->type = SUPPORTED_RATES_DISCRETE;
+  out_rates->discrete_count = 0;
+  for (size_t i = 0; i < STANDARD_RATES_COUNT; i++) {
+    if (snd_pcm_hw_params_test_rate(pcm, hwp, STANDARD_RATES[i], 0) == 0) {
+      out_rates->discrete[out_rates->discrete_count++] = STANDARD_RATES[i];
+    }
+  }
+  return true;
+}
+
+// Sample format map matching list_formats and alsa_format_to_str in upstream
+// (src/alsa_backend/utils.rs:397-431)
+static const struct {
+  snd_pcm_format_t pcm_fmt;
+  const char* str;
+} FORMAT_MAP[] = {
+    {SND_PCM_FORMAT_S16_LE, "S16_LE"},
+    {SND_PCM_FORMAT_S24_LE, "S24_4_LE"},
+    {SND_PCM_FORMAT_S24_3LE, "S24_3_LE"},
+    {SND_PCM_FORMAT_S32_LE, "S32_LE"},
+    {SND_PCM_FORMAT_FLOAT_LE, "F32_LE"},
+    {SND_PCM_FORMAT_FLOAT64_LE, "F64_LE"},
+    {SND_PCM_FORMAT_DSD_U8, "DSD_U8"},
+    {SND_PCM_FORMAT_DSD_U16_LE, "DSD_U16_LE"},
+    {SND_PCM_FORMAT_DSD_U16_BE, "DSD_U16_BE"},
+    {SND_PCM_FORMAT_DSD_U32_LE, "DSD_U32_LE"},
+    {SND_PCM_FORMAT_DSD_U32_BE, "DSD_U32_BE"},
+};
+#define FORMAT_MAP_COUNT (sizeof(FORMAT_MAP) / sizeof(FORMAT_MAP[0]))
+
+static size_t list_formats(snd_pcm_t* pcm, snd_pcm_hw_params_t* hwp,
+                           const char* out_formats[], size_t max_formats) {
+  size_t count = 0;
+  for (size_t i = 0; i < FORMAT_MAP_COUNT && count < max_formats; i++) {
+    if (snd_pcm_hw_params_test_format(pcm, hwp, FORMAT_MAP[i].pcm_fmt) == 0) {
+      out_formats[count++] = FORMAT_MAP[i].str;
+    }
+  }
+  return count;
+}
+
 // Queries ALSA device capabilities matching get_device_capabilities in upstream
 // (src/alsa_backend/utils.rs:165-253)
 audio_device_descriptor_t* alsa_capabilities_describe(const char* device_name,
@@ -184,18 +287,6 @@ audio_device_descriptor_t* alsa_capabilities_describe(const char* device_name,
   alsa_sanitize_device_name(device_name, clean_dev, sizeof(clean_dev));
 
   pthread_mutex_lock(&g_alsa_mutex);
-  audio_device_descriptor_t* desc =
-      (audio_device_descriptor_t*)calloc(1, sizeof(audio_device_descriptor_t));
-  if (!desc) {
-    if (err) {
-      device_error_init(err, DEVICE_ERROR_OTHER, "Out of memory");
-    }
-    pthread_mutex_unlock(&g_alsa_mutex);
-    return NULL;
-  }
-  snprintf(desc->name, sizeof(desc->name), "%s",
-           (device_name && device_name[0]) ? device_name : clean_dev);
-
   snd_pcm_stream_t stream =
       is_capture ? SND_PCM_STREAM_CAPTURE : SND_PCM_STREAM_PLAYBACK;
   snd_pcm_t* pcm = NULL;
@@ -213,9 +304,11 @@ audio_device_descriptor_t* alsa_capabilities_describe(const char* device_name,
         device_error_init(err, DEVICE_ERROR_OTHER, msg);
       }
     }
-    goto error_cleanup;
+    pthread_mutex_unlock(&g_alsa_mutex);
+    return NULL;
   }
 
+  // 1. let hwp = HwParams::any(&pcm) (src/alsa_backend/utils.rs:189-193)
   snd_pcm_hw_params_t* hwp = NULL;
   snd_pcm_hw_params_alloca(&hwp);
   if (snd_pcm_hw_params_any(pcm, hwp) < 0) {
@@ -223,15 +316,39 @@ audio_device_descriptor_t* alsa_capabilities_describe(const char* device_name,
       device_error_init(err, DEVICE_ERROR_OTHER,
                         "Failed to query ALSA hardware parameters");
     }
-    goto error_cleanup;
+    snd_pcm_close(pcm);
+    pthread_mutex_unlock(&g_alsa_mutex);
+    return NULL;
   }
 
-  unsigned int min_ch = 1, max_ch = 2;
-  snd_pcm_hw_params_get_channels_min(hwp, &min_ch);
-  snd_pcm_hw_params_get_channels_max(hwp, &max_ch);
-  unsigned int check_max = max_ch < CAPABILITY_PROBE_CHANNEL_LIMIT
-                               ? max_ch
-                               : CAPABILITY_PROBE_CHANNEL_LIMIT;
+  // 2. let channel_values = supported_channel_values(&hwp, CAPABILITY_PROBE_CHANNEL_LIMIT)
+  // (src/alsa_backend/utils.rs:194-199)
+  unsigned int channel_values[CAPABILITY_PROBE_CHANNEL_LIMIT];
+  int n_channels = supported_channel_values(
+      pcm, hwp, CAPABILITY_PROBE_CHANNEL_LIMIT, channel_values,
+      CAPABILITY_PROBE_CHANNEL_LIMIT);
+  if (n_channels <= 0) {
+    if (err) {
+      device_error_init(err, DEVICE_ERROR_OTHER,
+                        "Failed to query ALSA channel limits");
+    }
+    snd_pcm_close(pcm);
+    pthread_mutex_unlock(&g_alsa_mutex);
+    return NULL;
+  }
+
+  audio_device_descriptor_t* desc =
+      (audio_device_descriptor_t*)calloc(1, sizeof(audio_device_descriptor_t));
+  if (!desc) {
+    if (err) {
+      device_error_init(err, DEVICE_ERROR_OTHER, "Out of memory");
+    }
+    snd_pcm_close(pcm);
+    pthread_mutex_unlock(&g_alsa_mutex);
+    return NULL;
+  }
+  snprintf(desc->name, sizeof(desc->name), "%s",
+           (device_name && device_name[0]) ? device_name : clean_dev);
 
   desc->capability_sets_count = 1;
   desc->capability_sets =
@@ -241,96 +358,91 @@ audio_device_descriptor_t* alsa_capabilities_describe(const char* device_name,
   device_capability_set_t* set = &desc->capability_sets[0];
   snprintf(set->mode, sizeof(set->mode), "Unified");
 
-  size_t cap_alloc = (check_max >= min_ch) ? (check_max - min_ch + 1) : 1;
-  set->capabilities =
-      (channel_capability_t*)calloc(cap_alloc, sizeof(channel_capability_t));
+  set->capabilities = (channel_capability_t*)calloc(
+      (size_t)n_channels, sizeof(channel_capability_t));
   if (!set->capabilities) goto error_cleanup;
 
   size_t cap_idx = 0;
 
-  for (unsigned int ch = min_ch; ch <= check_max; ch++) {
-    if (snd_pcm_hw_params_test_channels(pcm, hwp, ch) != 0) {
-      continue;
-    }
+  // 3. for channels in channel_values (src/alsa_backend/utils.rs:202)
+  for (int c = 0; c < n_channels; c++) {
+    unsigned int channels = channel_values[c];
 
+    // if let Ok(hwp_ch) = HwParams::any(&pcm) && hwp_ch.set_channels(channels).is_ok()
+    //    && let Ok(rates_values) = list_samplerates(&hwp_ch)
+    // (src/alsa_backend/utils.rs:204-206)
     if (snd_pcm_hw_params_any(pcm, hwp) < 0 ||
-        snd_pcm_hw_params_set_channels(pcm, hwp, ch) < 0) {
+        snd_pcm_hw_params_set_channels(pcm, hwp, channels) < 0) {
       continue;
     }
 
-    unsigned int min_rate = 0, max_rate = 0;
-    snd_pcm_hw_params_get_rate_min(hwp, &min_rate, NULL);
-    snd_pcm_hw_params_get_rate_max(hwp, &max_rate, NULL);
+    supported_rates_t rates_values;
+    if (!list_samplerates(pcm, hwp, &rates_values)) {
+      continue;
+    }
 
-    bool is_range =
-        (min_rate != max_rate &&
-         snd_pcm_hw_params_test_rate(pcm, hwp, min_rate + 1, 0) == 0);
+    unsigned int rates[STANDARD_RATES_COUNT];
+    size_t rates_count = 0;
+    if (rates_values.type == SUPPORTED_RATES_DISCRETE) {
+      for (size_t r = 0; r < rates_values.discrete_count; r++) {
+        rates[rates_count++] = rates_values.discrete[r];
+      }
+    } else {
+      for (size_t r = 0; r < STANDARD_RATES_COUNT; r++) {
+        if (STANDARD_RATES[r] >= rates_values.min &&
+            STANDARD_RATES[r] <= rates_values.max) {
+          rates[rates_count++] = STANDARD_RATES[r];
+        }
+      }
+    }
 
     channel_capability_t* cap = &set->capabilities[cap_idx];
-    cap->channels = (int)ch;
+    cap->channels = (int)channels;
     cap->samplerates = (samplerate_capability_t*)calloc(
-        STANDARD_RATES_COUNT + 1, sizeof(samplerate_capability_t));
+        rates_count + 1, sizeof(samplerate_capability_t));
     if (!cap->samplerates) goto error_cleanup;
 
     size_t rate_idx = 0;
 
-    for (size_t r = 0; r < STANDARD_RATES_COUNT; r++) {
-      unsigned int test_rate = STANDARD_RATES[r];
-      if (is_range) {
-        if (test_rate < min_rate || test_rate > max_rate) continue;
-      } else if (min_rate == max_rate) {
-        if (test_rate != min_rate) continue;
-      } else {
-        if (snd_pcm_hw_params_test_rate(pcm, hwp, test_rate, 0) != 0) continue;
-      }
+    // 4. for rate in rates (src/alsa_backend/utils.rs:217)
+    for (size_t r = 0; r < rates_count; r++) {
+      unsigned int rate = rates[r];
 
+      // if let Ok(hwp_rate) = HwParams::any(&pcm)
+      //    && hwp_rate.set_channels(channels).is_ok()
+      //    && hwp_rate.set_rate(rate, alsa::ValueOr::Nearest).is_ok()
+      //    && hwp_rate.get_rate().ok() == Some(rate)
+      //    && let Ok(supported_formats) = list_formats(&hwp_rate)
+      // (src/alsa_backend/utils.rs:219-223)
       if (snd_pcm_hw_params_any(pcm, hwp) < 0 ||
-          snd_pcm_hw_params_set_channels(pcm, hwp, ch) < 0) {
+          snd_pcm_hw_params_set_channels(pcm, hwp, channels) < 0) {
         continue;
       }
 
-      unsigned int applied_rate = test_rate;
+      unsigned int applied_rate = rate;
       int dir = 0;
       if (snd_pcm_hw_params_set_rate_near(pcm, hwp, &applied_rate, &dir) < 0 ||
-          applied_rate != test_rate) {
+          applied_rate != rate) {
         continue;
       }
 
-      // Check formats matching list_formats in upstream
-      // (src/alsa_backend/utils.rs:408-431)
-      const snd_pcm_format_t test_fmts[] = {
-          SND_PCM_FORMAT_S16_LE,     SND_PCM_FORMAT_S24_LE,
-          SND_PCM_FORMAT_S24_3LE,    SND_PCM_FORMAT_S32_LE,
-          SND_PCM_FORMAT_FLOAT_LE,   SND_PCM_FORMAT_FLOAT64_LE,
-          SND_PCM_FORMAT_DSD_U8,     SND_PCM_FORMAT_DSD_U16_LE,
-          SND_PCM_FORMAT_DSD_U16_BE, SND_PCM_FORMAT_DSD_U32_LE,
-          SND_PCM_FORMAT_DSD_U32_BE};
-      const char* fmt_names[] = {"S16_LE",     "S24_4_LE",   "S24_3_LE",
-                                 "S32_LE",     "F32_LE",     "F64_LE",
-                                 "DSD_U8",     "DSD_U16_LE", "DSD_U16_BE",
-                                 "DSD_U32_LE", "DSD_U32_BE"};
-      const size_t num_test_fmts = sizeof(test_fmts) / sizeof(test_fmts[0]);
+      const char* supported_formats[FORMAT_MAP_COUNT];
+      size_t n_formats =
+          list_formats(pcm, hwp, supported_formats, FORMAT_MAP_COUNT);
+      if (n_formats == 0) {
+        continue;
+      }
 
       samplerate_capability_t* rate_cap = &cap->samplerates[rate_idx];
-      rate_cap->samplerate = (int)test_rate;
-      rate_cap->formats = (char**)calloc(num_test_fmts, sizeof(char*));
+      rate_cap->samplerate = (int)rate;
+      rate_cap->formats = (char**)calloc(n_formats, sizeof(char*));
       if (!rate_cap->formats) goto error_cleanup;
 
-      size_t fmt_idx = 0;
-      for (size_t f = 0; f < num_test_fmts; f++) {
-        if (snd_pcm_hw_params_test_format(pcm, hwp, test_fmts[f]) == 0) {
-          rate_cap->formats[fmt_idx++] = strdup(fmt_names[f]);
-        }
+      for (size_t f = 0; f < n_formats; f++) {
+        rate_cap->formats[f] = strdup(supported_formats[f]);
       }
-
-      if (fmt_idx > 0) {
-        rate_cap->formats_count = fmt_idx;
-        rate_idx++;
-      } else {
-        free(rate_cap->formats);
-        rate_cap->formats = NULL;
-        rate_cap->formats_count = 0;
-      }
+      rate_cap->formats_count = n_formats;
+      rate_idx++;
     }
 
     if (rate_idx > 0) {
